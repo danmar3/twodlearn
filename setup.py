8c1e12b6ddcaaf5c2370f9a22d1009e88f8a7099
Daniel L. Marino <daniellml55@gmail.com>
2019-08-21 18:02:40 -0400

    readme changed from markdown to rst format

--- setup.py
+++ setup.py
@@ -3,15 +3,24 @@ try:
 except ImportError:  # pip < 10.0
     from pip.operations import freeze
 from setuptools import setup, find_packages
+import pathlib
 # for development installation: pip install -e .
 # for distribution: python setup.py sdist #bdist_wheel
 #                   pip install dist/twodlearn_version.tar.gz
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
+# with open('README.md') as f:
+#     README = f.read()
+
+
 DEPS = ['pandas', 'pathlib', 'tqdm', 'matplotlib',
         'xarray', 'tensorflow-probability', 'tensorflow-datasets']
 
 
 def get_dependencies():
-    tf_names = ['tensorflow-gpu', 'tensorflow', 'tf-nightly']
+    tf_names = ['tensorflow-gpu>=1.13,<2', 'tensorflow>=1.13,<2', 'tf-nightly']
     tf_installed = any([any(tfname == installed.split('==')[0]
                             for tfname in tf_names)
                         for installed in freeze.freeze()])
@@ -22,7 +31,9 @@ def get_dependencies():
 
 
 setup(name='twodlearn',
-      version='0.5',
+      version='0.5.0',
+      description='Easy development of machine learning models',
+      long_description=README,
       packages=find_packages(
           exclude=["*test*", "tests"]),
       # package_data={
@@ -33,7 +44,8 @@ setup(name='twodlearn',
       python_requires='>=3.5.2',
       extras_require={
           'reinforce': ['gym', 'pybullet==2.4.5', 'casadi'],
-          'development': ['pytest', 'line_profiler', 'pytest-faulthandler'],
+          'development': ['pytest', 'line_profiler', 'pytest-faulthandler',
+                          'jupyter'],
       },
       author='Daniel L. Marino',
       author_email='marinodl@vcu.edu',
