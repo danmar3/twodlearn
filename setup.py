ba0752c721bd3afd27da8908299876c89b3af3b8
Daniel L. Marino <daniellml55@gmail.com>
2019-08-29 21:46:56 -0400

    throwing error when unsupported version of tensorflow is installed.

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 try:
     from pip._internal.operations import freeze
+    from pip._internal.exceptions import InstallationError
 except ImportError:  # pip < 10.0
     from pip.operations import freeze
+    from pip.exceptions import InstallationError
 import operator
 from setuptools import setup, find_packages
 import pathlib
@@ -20,14 +22,15 @@ DEPS = ['pandas', 'pathlib', 'tqdm', 'matplotlib',
         'xarray', 'tensorflow-probability', 'tensorflow-datasets']
 
 
+def split_name_ver(input):
+    '''split string in name and version'''
+    sep = min(input.find(op) if input.find(op) > 0 else len(input)
+              for op in ('=', '>', '<'))
+    return input[:sep], input[sep:]
+
+
 def check_dep(installed, target):
     '''check if installed '''
-    def split_name_ver(input):
-        '''split string in name and version'''
-        sep = min(input.find(op) if input.find(op) > 0 else len(input)
-                  for op in ('=', '>', '<'))
-        return input[:sep], input[sep:]
-
     def split_ver_op(input):
         '''split string in version and operation'''
         ops = {'==': operator.eq, '>=': operator.ge, '<=': operator.le,
@@ -65,12 +68,23 @@ def check_dep(installed, target):
 
 
 def get_dependencies():
-    tf_names = ['tensorflow-gpu>=1.13,<2', 'tensorflow>=1.13,<2', 'tf-nightly']
-    tf_installed = any([any(check_dep(installed=installed, target=tfname)
-                            for tfname in tf_names)
-                        for installed in freeze.freeze()])
+    tf_names = ['tensorflow-gpu>=1.14,<2', 'tensorflow>=1.14,<2', 'tf-nightly']
+    tf_installed = any([
+        any(installed.split('==')[0] == split_name_ver(tfname)[0]
+            for tfname in tf_names)
+        for installed in freeze.freeze()])
     if tf_installed:
-        return DEPS
+        tf_check = any([
+            any(check_dep(installed=installed, target=tfname)
+                for tfname in tf_names)
+            for installed in freeze.freeze()
+            if '==' in installed])
+        if tf_check:
+            return DEPS
+        else:
+            raise InstallationError(
+                'Unsupported version of tensorflow is installed. '
+                'Supported versions are {}'.format(tf_names))
     else:
         return DEPS + ['tensorflow']
 
