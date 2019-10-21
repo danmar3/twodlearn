d1086a40c0e4cc0ea923c73cd603f71764bb9d54
Daniel L. Marino <daniellml55@gmail.com>
2019-10-21 11:58:33 -0400

    added resnet test

--- twodlearn/tests/resnet_test.py
+++ twodlearn/tests/resnet_test.py
@@ -24,6 +24,26 @@ class ResnetTest(unittest.TestCase):
         # trainable
         assert len(tdl.core.get_trainable(reslayer)) == 6
 
+    def test_get_trainable2(self):
+        residual = tdl.stacked.StackedLayers(
+           layers=[tf.keras.layers.Conv2D(kernel_size=[5, 5], filters=5,
+                                          padding='same'),
+                   tf.keras.layers.Conv2D(kernel_size=[10, 10], filters=1,
+                                          padding='same'),
+                   ])
+        reslayer = tdl.resnet.ResConv2D(residual=residual)
+        input = tf.keras.layers.Input(shape=(28, 28, 1), name='the_input')
+        # output_shape
+        output_shape = reslayer.compute_output_shape(input.shape)
+        assert output_shape.as_list() == [None, 28, 28, 1]
+
+        # output
+        output = reslayer(input)
+        assert output.shape.as_list() == [None, 28, 28, 1]
+
+        # trainable
+        assert len(tdl.core.get_trainable(reslayer)) == 4
+
 
 if __name__ == "__main__":
     unittest.main()
